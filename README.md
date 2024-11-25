4d6fca6d4b41f35ea514aa3d1b02ff37c61f1161
Open Source SW <m.s54278169@gmail.com>
2024-11-25 14:56:53 +0900

    Need to a new function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # practicerepo
 ## study git
+Git is a tool about version
+control for github
---
+++ newfeature.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main() {
+// need to print current working dir.
+// please develop this function.
+}
